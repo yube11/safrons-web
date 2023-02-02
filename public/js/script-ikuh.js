75739dd08d8c0c081c439df015b2b01c126224b2
yube11 <yudha_11@apps.ipb.ac.id>
2023-02-02 23:47:22 +0700

    harabogor

---
+++ public/js/script-ikuh.js
@@ -0,0 +1,7 @@
+
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+searchButton.addEventListener('click', () => {
+  const inputValue = searchInput.value;
+  alert(inputValue);
+});
